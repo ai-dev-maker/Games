2cabdf91bf605b3d03ba9dd214f9ebd912ea32b6
AI Tech <132939290+DmitryyIT@users.noreply.github.com>
2024-03-10 15:43:43 +0200

    Update shooter_game.py

--- shooter_game.py
+++ shooter_game.py
@@ -2,6 +2,8 @@ from pygame import *
 from random import *
 from time import time as timer
 
+mixer.init()
+
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_width, player_height, speed=0):
@@ -11,18 +13,6 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-
-    def manage_sprite(self):
-        keys = key.get_pressed()
-
-        if keys[K_w] and self.rect.y > 0:
-            self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 450:
-            self.rect.y += self.speed
-        if keys[K_a] and self.rect.x > 0:
-            self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x < 650:
-            self.rect.x += self.speed
 
     def reset(self):
         window.blit(self.image, self.rect)
@@ -45,15 +35,31 @@ class Player(GameSprite):
         # self.rect.x = pos[0]
         # self.rect.y = pos[1]
 
+    def fire(self):
+        bullet = Bullet("images/bullet.png", self.rect.centerx-6, self.rect.y, 10, 14, 10)
+        Bullets.add(bullet)
+        # f = mixer.Sound("music/fire.ogg")
+        # f.play()
+
 
 class Enemy(GameSprite):
     def update(self):
+        global lost
         self.rect.y += self.speed
 
         if self.rect.y > window_height + 100:
             self.rect.y = -100
             self.rect.x = randint(10, window_width - 50)
             self.speed = randint(2, 4)
+            lost += 1
+
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed
+
+        if self.rect.y < -50:
+            self.kill()
 
 
 window_width, window_height = 900, 700
@@ -65,10 +71,10 @@ background = transform.scale(image.load('images/galaxy.jpg'), (window_width, win
 
 font.init()
 font2 = font.Font(None, 50)
+font3 = font.SysFont('Georgia', 20)
 win = font2.render("You win", True, (0, 200, 0))
 lose = font2.render("You lose", True, (200, 0, 0))
 game = True
-
 
 player = Player("images/rocket.png", window_width / 2 - 70, window_height - 100, 70, 90, 6)
 
@@ -81,11 +87,19 @@ for i in range(5):
 
 clock = time.Clock()
 
+Bullets = sprite.Group()
+
+score = 0
+lost = 0
+live = 10
+
 run = True
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        if e.type == MOUSEBUTTONDOWN and e.button == 1:
+            player.fire()
 
     if run:
         window.blit(background, (0, 0))
@@ -95,6 +109,31 @@ while game:
 
         monsters.update()
         monsters.draw(window)
+
+        Bullets.update()
+        Bullets.draw(window)
+
+        score_text = font3.render("Score: " + str(score), True, (255, 255, 255))
+        window.blit(score_text, (20, 20))
+        lost_text = font3.render("Lost: " + str(lost), True, (255, 255, 255))
+        window.blit(lost_text, (20, 45))
+        live_text = font3.render("Live: " + str(live), True, (150, 0, 0))
+        window.blit(live_text, (20, 70))
+
+        collide = sprite.groupcollide(Bullets, monsters, True, True)
+        for i in collide:
+            score += 1
+            monster = Enemy(choice(monsters_png), randint(0, window_width), -100, 50, 50, randint(1, 2))
+            monsters.add(monster)
+
+        collide_2 = sprite.spritecollide(player, monsters, True)
+        for i in collide_2:
+            if live > 1:
+                live -= 1
+                monster = Enemy(choice(monsters_png), randint(0, window_width), -100, 50, 50, randint(1, 2))
+                monsters.add(monster)
+            else:
+                run = False
 
     display.update()
     clock.tick(60)
