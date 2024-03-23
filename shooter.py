c67a6ad5a3d91d6e6a4053f2c0979d5c16d4092e
AI Tech <132939290+ai-dev-maker@users.noreply.github.com>
2024-03-23 20:39:38 +0200

    Update shooter.py

--- shooter.py
+++ shooter.py
@@ -1,10 +1,18 @@
 from pygame import *
 from random import *
 from time import time as timer
+import sys
+
+
+window_width, window_height = 900, 700
+sprite_width, sprite_height = 70, 50
+window = display.set_mode((window_width, window_height))
+display.set_caption('Shooter')
 
 mixer.init()
 
 FPS = 60
+clock = time.Clock()
 
 
 class Button:
@@ -15,19 +23,19 @@ class Button:
         self.active_color = active_color
         self.display = display
 
-    def draw(self, display,  x, y, message, action=None, font_size=35):
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-        if x < mouse[0] < x + self.width and y < mouse[1] < y + self.height:
-            pygame.draw.rect(display, self.active_color, (x, y, self.width, self.height))
+    def draw(self, window, x, y, message, action=None, font_size=35):
+        mouse_ = mouse.get_pos()
+        click = mouse.get_pressed()
+        if x < mouse_[0] < x + self.width and y < mouse_[1] < y + self.height:
+            draw.rect(window, self.active_color, (x, y, self.width, self.height))
             if click[0] == 1:
-                pygame.time.delay(3)
+                time.delay(3)
                 if action is not None:
                     action()
         else:
-            pygame.draw.rect(display, self.inactive_color, (x, y, self.width, self.height))
+            draw.rect(window, self.inactive_color, (x, y, self.width, self.height))
 
-        print_text(display, message=message, x=x+10, y=y+10, font_color=(0, 0, 0), font_size=font_size)
+        print_text(window, message=message, x=x+10, y=y+10, font_color=(0, 0, 0), font_size=font_size)
 
     def is_clicked(self, mouse_pos):
         button_rect = pygame.Rect(0, 0, self.width, self.height)
@@ -108,115 +116,142 @@ class Gun(GameSprite):
 
 
 class Animation(sprite.Sprite):
-    def __init__(self, name_dir, pos_x, pos_y, count_sprite):
+    def __init__(self, name_dir_anim, pos_x, pos_y, count_sprite):
         super().__init__()
-        self.anim_set = [transform.scale(image.load(f"{name_dir}/{i}.png"), (100, 100)) for i in range(count_sprite)]
+        self.animation_set = [transform.scale(image.load(f"{name_dir_anim}/{i}.png"), (50, 50)) for i in range(1, count_sprite)]
+        self.i = 0
         self.x = pos_x
         self.y = pos_y
-        self.cadr = 0
 
     def update(self):
-        window.blit(self.anim_set[self.cadr], sefl.x, self.y)
-        cadr += 1
-
-        if self.cadr >= len(self.anim_set):
+        window.blit(self.animation_set[self.i], (self.x, self.y))
+        self.i += 1
+        if self.i > len(self.animation_set) - 1:
             self.kill()
 
-def print_text(display, message, x, y, font_color, font_type=None, font_size=35):
-    font_type = pygame.font.Font(font_type, font_size)
+
+def print_text(window, message, x, y, font_color, font_type=None, font_size=35):
+    font_type = font.Font(font_type, font_size)
     text = font_type.render(message, True, font_color)
-    display.blit(text, (x, y))
+    window.blit(text, (x, y))
 
 
-window_width, window_height = 900, 700
-sprite_width, sprite_height = 70, 50
-window = display.set_mode((window_width, window_height))
-display.set_caption('Shooter')
+def game_menu():
+    font.init()
+    menu_bg = image.load("images/city_menu.png")
+    menu_bg = transform.scale(menu_bg, (window.get_width(), window.get_height()))
 
-background = transform.scale(image.load('images/galaxy.jpg'), (window_width, window_height))
+    play_btn = Button(230, 60, (255, 152, 255), (255, 104, 255), window)
+    quit_btn = Button(110, 60, (255, 152, 255), (255, 104, 255), window)
 
-font.init()
-font2 = font.Font(None, 50)
-font3 = font.SysFont('Georgia', 20)
-win = font2.render("You win", True, (0, 200, 0))
-lose = font2.render("You lose", True, (200, 0, 0))
-game = True
+    show = True
+    while show:
+        for e in event.get():
+            if e.type == QUIT:
+                show = False
 
-player = Player("images/rocket.png", window_width / 2 - 70, window_height - 100, 70, 90, 6)
-gun1 = Gun("images/rocket.png", "images/bullet.png", player, 50, 70, 6)
-gun2 = Gun("images/rocket.png", "images/bullet.png", player, 50, 70, 6)
+        window.blit(menu_bg, (0, 0))
 
+        play_btn.draw(window, (window_width / 2) - 90, (window_height / 2) - 50, "Play Game", game_run, 57)
+        quit_btn.draw(window, (window_width / 2) - 30, (window_height / 2) + 50, "Quit", close_window, 57)
 
-monsters = sprite.Group()
-monsters_png = ["images/ufo.png", "images/asteroid.png"]
-for i in range(5):
-    monster = Enemy(choice(monsters_png), randint(0, window_width), -100, 50, 50, randint(1, 2))
-    monsters.add(monster)
+        display.update()
+        clock.tick(FPS)
 
 
-clock = time.Clock()
+def game_run():
+    global Bullets
+    global lost
 
-Bullets = sprite.Group()
+    background = transform.scale(image.load('images/galaxy.jpg'), (window_width, window_height))
 
-anim_hit = sprite.Group()
+    font.init()
+    font2 = font.Font(None, 50)
+    font3 = font.SysFont('Georgia', 20)
+    win = font2.render("You win", True, (0, 200, 0))
+    lose = font2.render("You lose", True, (200, 0, 0))
+    game = True
 
-score = 0
-lost = 0
-live = 10
+    player = Player("images/rocket.png", window_width / 2 - 70, window_height - 100, 70, 90, 6)
+    gun1 = Gun("images/rocket.png", "images/bullet.png", player, 50, 70, 6)
+    gun2 = Gun("images/rocket.png", "images/bullet.png", player, 50, 70, 6)
 
-run = True
-while game:
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-        if e.type == MOUSEBUTTONDOWN and e.button == 1:
-            player.fire()
-            # gun1.fire()
-            # gun2.fire()
+    monsters = sprite.Group()
+    monsters_png = ["images/ufo.png", "images/asteroid.png"]
+    for i in range(5):
+        monster = Enemy(choice(monsters_png), randint(0, window_width), -100, 50, 50, randint(1, 2))
+        monsters.add(monster)
 
-    if run:
-        window.blit(background, (0, 0))
+    Bullets = sprite.Group()
 
-        player.update()
-        player.reset()
+    anim_hit = sprite.Group()
 
-        gun1.update(-30, -15)
-        gun2.update(50, -15)
+    score = 0
+    lost = 0
+    live = 10
 
-        monsters.update()
-        monsters.draw(window)
+    run = True
+    while game:
+        for e in event.get():
+            if e.type == QUIT:
+                game = False
+            if e.type == MOUSEBUTTONDOWN and e.button == 1:
+                player.fire()
+                # gun1.fire()
+                # gun2.fire()
 
-        Bullets.update()
-        Bullets.draw(window)
+        if run:
+            window.blit(background, (0, 0))
 
-        anim_hit.update()
+            player.update()
+            player.reset()
 
-        score_text = font3.render("Score: " + str(score), True, (255, 255, 255))
-        window.blit(score_text, (20, 20))
-        lost_text = font3.render("Lost: " + str(lost), True, (255, 255, 255))
-        window.blit(lost_text, (20, 45))
-        live_text = font3.render("Live: " + str(live), True, (150, 0, 0))
-        window.blit(live_text, (20, 70))
+            gun1.update(-30, -15)
+            gun2.update(50, -15)
 
-        collide = sprite.groupcollide(Bullets, monsters, True, True)
-        for i in collide:
-            score += 1
-            monster = Enemy(choice(monsters_png), randint(0, window_width), -100, 50, 50, randint(1, 2))
-            monsters.add(monster)
+            monsters.update()
+            monsters.draw(window)
 
-            x, y = c.rect.x-10, c.rect.y-10
-            hit = Animation(None, x, y, 20)
-            anim_hit.add(hit)
+            Bullets.update()
+            Bullets.draw(window)
 
-        collide_2 = sprite.spritecollide(player, monsters, True)
-        for i in collide_2:
-            if live > 1:
-                live -= 1
+            anim_hit.update()
+
+            score_text = font3.render("Score: " + str(score), True, (255, 255, 255))
+            window.blit(score_text, (20, 20))
+            lost_text = font3.render("Lost: " + str(lost), True, (255, 255, 255))
+            window.blit(lost_text, (20, 45))
+            live_text = font3.render("Live: " + str(live), True, (150, 0, 0))
+            window.blit(live_text, (20, 70))
+
+            collide = sprite.groupcollide(Bullets, monsters, True, True)
+            for c in collide:
+                score += 1
                 monster = Enemy(choice(monsters_png), randint(0, window_width), -100, 50, 50, randint(1, 2))
                 monsters.add(monster)
-            else:
-                run = False
 
-    display.update()
-    clock.tick(FPS)
+                x, y = c.rect.x-10, c.rect.y-10
+                hit = Animation('images/anim11', x, y, 19)
+                anim_hit.add(hit)
+
+            collide_2 = sprite.spritecollide(player, monsters, True)
+            for i in collide_2:
+                if live > 1:
+                    live -= 1
+                    monster = Enemy(choice(monsters_png), randint(0, window_width), -100, 50, 50, randint(1, 2))
+                    monsters.add(monster)
+                else:
+                    run = False
+
+        display.update()
+        clock.tick(FPS)
+
+
+def close_window():
+    click = mouse.get_pressed()
+    if click[0] == 1:
+        sys.exit()
+
+
+game_menu()
 
