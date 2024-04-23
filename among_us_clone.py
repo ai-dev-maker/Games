e70c1d1ec4a3a737eb5124581df483b76eac7225
AI Tech <132939290+ai-dev-maker@users.noreply.github.com>
2024-04-23 21:39:51 +0300

    Update among_us_clone.py

--- among_us_clone.py
+++ among_us_clone.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import time
 
 pygame.init()
 
@@ -15,7 +16,10 @@ FPS = 60
 sprite_running_right = None
 sprite_running_left = None
 
-width_sprite, height_sprite = 65, 65
+width_sprite, height_sprite = 50, 60
+
+run_right = [pygame.image.load("images/sprites/red_right0.png"), pygame.image.load("images/sprites/red_right1.png"),
+             pygame.image.load("images/sprites/red_right2.png"), pygame.image.load("images/sprites/red_right3.png")]
 
 
 class Button:
@@ -26,23 +30,30 @@ class Button:
         self.active_color = active_color
         self.display = display
 
-    def draw(self, window, x, y, message, action=None, font_size=35):
-        mouse_ = mouse.get_pos()
-        click = mouse.get_pressed()
-        if x < mouse_[0] < x + self.width and y < mouse_[1] < y + self.height:
-            draw.rect(window, self.active_color, (x, y, self.width, self.height))
+    def draw(self, display,  x, y, message, action=None, font_size=35):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if x < mouse[0] < x + self.width and y < mouse[1] < y + self.height:
+            pygame.draw.rect(display, self.active_color, (x, y, self.width, self.height))
             if click[0] == 1:
-                time.delay(3)
+                # pygame.mixer.Sound.play(button_sound)
+                pygame.time.delay(3)
                 if action is not None:
                     action()
         else:
-            draw.rect(window, self.inactive_color, (x, y, self.width, self.height))
+            pygame.draw.rect(display, self.inactive_color, (x, y, self.width, self.height))
 
-        print_text(window, message=message, x=x+10, y=y+10, font_color=(0, 0, 0), font_size=font_size)
+        print_text(display, message=message, x=x+10, y=y+10, font_color=(0, 0, 0), font_size=font_size)
 
     def is_clicked(self, mouse_pos):
         button_rect = pygame.Rect(0, 0, self.width, self.height)
         return button_rect.collidepoint(mouse_pos)
+
+
+def print_text(window, message, x, y, font_color, font_type="font/game_font.ttf", font_size=35):
+    font_type = pygame.font.Font(font_type, font_size)
+    text = font_type.render(message, True, font_color)
+    window.blit(text, (x, y))
 
 
 class GameSprite(pygame.sprite.Sprite):
@@ -63,27 +74,54 @@ class Player(GameSprite):
         super().__init__(player_image, map_x, map_y, player_width, player_height, speed)
         self.map_x = map_x
         self.map_y = map_y
+        self.left = False
+        self.right = False
+        self.stand = True
+        self.anim_count = 0
 
     def update(self):
-        global sprite_running_right, sprite_running_left
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.map_x += self.speed
-            sprite_running_left = True
+            self.left = True
+            self.right = False
+            self.stand = False
         elif keys[pygame.K_d]:
             self.map_x -= self.speed
-            sprite_running_right = True
+            self.right = True
+            self.left = False
+            self.stand = False
         if keys[pygame.K_w]:
             self.map_y += self.speed
-            sprite_running_left = True
+            self.right = False
+            self.left = False
+            self.stand = True
         elif keys[pygame.K_s]:
             self.map_y -= self.speed
-            sprite_running_right = True
-        elif keys[pygame.K_g]:
-            print(self.map_x, self.map_y)
+            self.right = False
+            self.left = False
+            self.stand = True
+        else:
+            self.right = False
+            self.left = False
+            self.stand = True
+            self.anim_count = 0
 
         self.rect.x = (WIDTH // 2)
         self.rect.y = (HEIGHT // 2)
+
+    def reset(self):
+        if self.anim_count + 1 >= FPS:
+            self.anim_count = 0
+
+        if self.stand:
+            screen.blit(self.image, self.rect)
+        if self.right:
+            screen.blit(run_right[self.anim_count // 4], self.rect)
+        if self.left:
+            pass
+
+        self.anim_count += 1
 
 
 class Animation(pygame.sprite.Sprite):
@@ -119,11 +157,9 @@ def game_run():
 
     scale_factor = 0.5
     scaled_map = pygame.transform.scale(map_image, (map_image.get_width() * scale_factor, map_image.get_height() * scale_factor))
-    player = Player('images/sprites/sprite.png', -1825, -136, width_sprite, height_sprite, 5)
+    player = Player('images/sprites/red_sprite.png', -1825, -136, width_sprite, height_sprite, 5)
 
-    wall1 = Wall(20, 200, -1825, -136, (100, 0, 0))
-
-    walls = [wall1]
+    wall1 = Wall(20, 200, -1825, -136, (200, 0, 0))
 
     running = True
     while running:
@@ -136,8 +172,7 @@ def game_run():
         player.reset()
         player.update()
 
-        for wall in walls:
-            wall.reset()
+        wall1.reset()
 
         pygame.display.flip()
         clock.tick(FPS)
@@ -150,6 +185,8 @@ def game_menu():
     map_image = pygame.image.load("images/map/menu.webp")
     transform_map = pygame.transform.scale(map_image, (WIDTH, HEIGHT))
 
+    play_btn = Button(105, 45, (100, 0, 0), (150, 0, 0), screen)
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -157,6 +194,8 @@ def game_menu():
                 running = False
 
         screen.blit(transform_map, (0, 0))
+
+        play_btn.draw(screen, WIDTH // 2 - 50, HEIGHT // 2 - 25, "Play", game_run, 30)
 
         pygame.display.flip()
         clock.tick(FPS)
