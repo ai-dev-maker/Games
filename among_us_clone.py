f1c8e01aff759835c63851b5cb336aae8dcc4727
AI Tech <132939290+ai-dev-maker@users.noreply.github.com>
2024-05-07 20:49:18 +0300

    Update among_us_clone.py

--- among_us_clone.py
+++ among_us_clone.py
@@ -1,25 +1,497 @@
+import pygame
+import sys
+import time
+import math
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+SCENE_WIDTH = 8565
+SCENE_HEIGHT = 4794
+
+SCREEN_SIZE = (WIDTH, HEIGHT)
+
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("Among Us Clone")
+
+clock = pygame.time.Clock()
+FPS = 60
+
+sprite_running_right = None
+sprite_running_left = None
+
+width_sprite, height_sprite = 40, 50
+
+run_right = [pygame.image.load("images/sprites/red_right0.png"), pygame.image.load("images/sprites/red_right1.png"),
+             pygame.image.load("images/sprites/red_right2.png"), pygame.image.load("images/sprites/red_right3.png")]
+
+
+class Button:
+    def __init__(self, width, height, inactive_color, active_color, display):
+        self.width = width
+        self.height = height
+        self.inactive_color = inactive_color
+        self.active_color = active_color
+        self.display = display
+
+    def draw(self, display, x, y, message, action=None, font_size=35):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if x < mouse[0] < x + self.width and y < mouse[1] < y + self.height:
+            pygame.draw.rect(display, self.active_color, (x, y, self.width, self.height))
+            if click[0] == 1:
+                # pygame.mixer.Sound.play(button_sound)
+                pygame.time.delay(100)
+                if action is not None:
+                    action()
+        else:
+            pygame.draw.rect(display, self.inactive_color, (x, y, self.width, self.height))
+
+        print_text(display, message=message, x=x + 10, y=y + 10, font_color=(0, 0, 0), font_size=font_size)
+
+    def is_clicked(self, mouse_pos):
+        button_rect = pygame.Rect(0, 0, self.width, self.height)
+        return button_rect.collidepoint(mouse_pos)
+
+
+def print_text(window, message, x, y, font_color, font_type="font/game_font.ttf", font_size=35):
+    font_type = pygame.font.Font(font_type, font_size)
+    text = font_type.render(message, True, font_color)
+    window.blit(text, (x, y))
+
+
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_width, player_height, speed=0):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(player_image), (player_width, player_height))
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        self.width_sprite = width_sprite
+        self.height_sprite = height_sprite
+        self.counter = 0
+
+    def animation(self, kind):
+        if kind == 'stay_right':
+            self.image = pygame.transform.scale(pygame.image.load("images/sprites/red_sprite.png"),
+                                                    (self.width_sprite, self.height_sprite))
+
+        elif kind == 'right':
+            self.counter += 1
+            if 0 <= self.counter < 20:
+                self.image = pygame.transform.scale(pygame.image.load("images/sprites/red_right0.png"),
+                                                        (self.width_sprite, self.height_sprite))
+            elif 20 <= self.counter < 25:
+                self.image = pygame.transform.scale(pygame.image.load("images/sprites/red_right1.png"),
+                                                        (self.width_sprite, self.height_sprite))
+            elif 25 <= self.counter < 45:
+                self.image = pygame.transform.scale(pygame.image.load("images/sprites/red_right2.png"),
+                                                        (self.width_sprite, self.height_sprite))
+            elif 45 <= self.counter < 50:
+                self.image = pygame.transform.scale(pygame.image.load("images/sprites/red_right3.png"),
+                                                        (self.width_sprite, self.height_sprite))
+
+            if self.counter > 50:
+                self.counter = 0
+
+        if kind == 'stay_left':
+            self.image = pygame.transform.scale(pygame.image.load("images/sprites/stay_left.png"),
+                                                    (self.width_sprite, self.height_sprite))
+        elif kind == 'left':
+            self.counter += 1
+            if 0 <= self.counter < 20:
+                self.image = pygame.transform.scale(pygame.image.load("images/sprites/red_left0.png"),
+                                                        (self.width_sprite, self.height_sprite))
+            elif 20 <= self.counter < 25:
+                self.image = pygame.transform.scale(pygame.image.load("images/sprites/red_left1.png"),
+                                                        (self.width_sprite, self.height_sprite))
+            elif 25 <= self.counter < 45:
+                self.image = pygame.transform.scale(pygame.image.load("images/sprites/red_left2.png"),
+                                                        (self.width_sprite, self.height_sprite))
+            elif 45 <= self.counter < 50:
+                self.image = pygame.transform.scale(pygame.image.load("images/sprites/red_left1.png"),
+                                                        (self.width_sprite, self.height_sprite))
+
+            if self.counter > 50:
+                self.counter = 0
+
+        elif kind == 'dead':
+            self.image = pygame.transform.scale(pygame.image.load("images/sprites/white.png"),
+                                                (self.width_sprite, self.height_sprite))
+
+    def reset(self):
+        screen.blit(self.image, self.rect)
+
+
+class Player(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_width, player_height, speed=0):
+        super().__init__(player_image, player_x, player_y, player_width, player_height, 5)
+        self.counter = 0
+
+    def update(self):
+        global cam_x
+        global cam_y
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            if self.rect.y < HEIGHT // 2 - height_sprite and cam_y + self.speed <= 0:
+                cam_y += self.speed
+            elif self.rect.y > 10:
+                if not self.check_collision(0, -self.speed):
+                    self.rect.y -= self.speed
+            self.animation(kind='right')
+
+        elif keys[pygame.K_s]:
+            if self.rect.y > HEIGHT // 2 - height_sprite and cam_y - self.speed >= HEIGHT - SCENE_HEIGHT:
+                cam_y -= self.speed
+            elif self.rect.y < HEIGHT - 50:
+                if not self.check_collision(0, self.speed):
+                    self.rect.y += self.speed
+            self.animation(kind='left')
+
+        elif keys[pygame.K_a]:
+            if self.rect.x < WIDTH // 2 and cam_x + self.speed <= 0:
+                cam_x += self.speed
+            elif self.rect.x > 0:
+                if not self.check_collision(-self.speed, 0):
+                    self.rect.x -= self.speed
+            self.animation(kind='left')
+
+        elif keys[pygame.K_d]:
+            if self.rect.x > WIDTH // 2 and cam_x - self.speed >= WIDTH - SCENE_WIDTH:
+                cam_x -= self.speed
+            elif self.rect.x < WIDTH - 50:
+                if not self.check_collision(self.speed, 0):
+                    self.rect.x += self.speed
+            self.animation(kind='right')
+
+        else:
+            self.animation(kind='stay_right')
+
+        if keys[pygame.K_g]:
+            print(self.rect.x, self.rect.y)
+
+    def check_collision(self, x_shift, y_shift):
+        new_rect = self.rect.move(x_shift, y_shift)
+        for wall in GroupWall:
+            # wall.reset()
+
+            wall_closest_x = min(max(new_rect.x, wall.rect.x), wall.rect.x + wall.rect.width)
+            wall_closest_y = min(max(new_rect.y, wall.rect.y), wall.rect.y + wall.rect.height)
+
+            distance = math.sqrt((new_rect.x - wall_closest_x) ** 2 + (new_rect.y - wall_closest_y) ** 2)
+
+            if distance <= float(0):
+                return True
+
+            if distance <= self.speed:
+                return True
+
+            if new_rect.colliderect(wall.rect):
+                return True
+
+        return False
+
+
+class Wall(pygame.sprite.Sprite):
+
+    def __init__(self, wall_width, wall_height, wall_x, wall_y, color):
+        super().__init__()
+        self.image = pygame.Surface((wall_width, wall_height))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+
+        self.local_x = wall_x
+        self.local_y = wall_y
+
+    def update(self):
+        self.rect.x = (self.local_x + cam_x)
+        self.rect.y = (self.local_y + cam_y)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_h]:
+            print(self.rect.x, self.rect.y)
+
+        # print(self.rect)
+
+        screen.blit(self.image, self.rect)
+
+    def reset(self):
+        screen.blit(self.image, self.rect)
+
+
+class Crewmate(GameSprite):
+    def __init__(self, bot_image, bot_x, bot_y, bot_width, bot_height, speed=0):
+        super().__init__(bot_image, bot_x, bot_y, bot_width, bot_height, 5)
+        self.counter = 0
+
+    def update(self, player_rect):
+        different_x = player_rect.x - self.rect.x
+        different_y = player_rect.y - self.rect.y
+
+        if abs(different_x) > abs(different_y):
+            if different_x > 0:
+                self.rect.x += self.speed - 3
+                self.animation(kind="right")
+            else:
+                self.rect.x -= self.speed - 3
+                self.animation(kind="left")
+        else:
+            if different_y > 0:
+                self.rect.y += self.speed - 3
+            else:
+                self.rect.y -= self.speed - 3
+
+
 class Impostor(GameSprite):
     def __init__(self, impostor_image, impostor_x, impostor_y, impostor_width, impostor_height, speed=0):
         super().__init__(impostor_image, impostor_x, impostor_y, impostor_width, impostor_height, 5)
         self.counter = 0
 
-    def update(self, bot_sprites):
-        for bot in bot_sprites:
-            # Рассчитываем расстояние между центрами спрайтов
-            different_x = bot.rect.centerx - self.rect.centerx
-            different_y = bot.rect.centery - self.rect.centery
+    def update(self, bot_rect, player_rect):
+        different_bot_x = bot_rect.x - self.rect.x
+        different_bot_y = bot_rect.y - self.rect.y
 
-            if abs(different_x) > abs(different_y):
-                if different_x > 0:
-                    self.rect.x += self.speed - 2
-                    self.animation(kind="right")
-                else:
-                    self.rect.x -= self.speed - 2
-                    self.animation(kind="left")
+        different_player_x = player_rect.x - self.rect.x
+        different_player_y = player_rect.y - self.rect.x
+
+        if abs(different_bot_x) and abs(different_bot_y) < abs(different_player_x) and abs(different_player_y):
+            if different_player_x > 0:
+                self.rect.x += self.speed - 3
+                self.animation(kind="right")
             else:
-                if different_y > 0:
-                    self.rect.y += self.speed - 2
-                else:
-                    self.rect.y -= self.speed - 2
-                bot.speed = 0
-                bot.animation(kind='dead')
+                self.rect.x -= self.speed - 3
+                self.animation(kind="left")
+
+            if different_player_y > 0:
+                self.rect.y += self.speed - 3
+            else:
+                self.rect.y -= self.speed - 3
+        else:
+            if different_bot_x > 0:
+                self.rect.x += self.speed - 3
+                self.animation(kind="right")
+            else:
+                self.rect.x -= self.speed - 3
+                self.animation(kind="left")
+
+            if different_bot_y > 0:
+                self.rect.y += self.speed - 3
+            else:
+                self.rect.y -= self.speed - 3
+
+
+GroupWall = pygame.sprite.Group()
+
+wall1 = Wall(770, 13, 1157, 480, (255, 0, 255))
+wall2 = Wall(13, 192, 1153, 585, (255, 0, 255))
+wall3 = Wall(447, 13, 1153, 585, (255, 0, 255))
+wall4 = Wall(13, 132, 1598, 585, (255, 0, 255))
+wall5 = Wall(132, 13, 1478, 705, (255, 0, 255))
+wall6 = Wall(13, 423, 1477, 635, (255, 0, 255))
+wall7 = Wall(13, 240, 1868, 640, (255, 0, 255))
+wall8 = Wall(13, 215, 1922, 588, (255, 0, 255))
+wall9 = Wall(13, 295, 1922, 198, (255, 0, 255))
+wall10 = Wall(195, 13, 1740, 586, (255, 0, 255))
+wall11 = Wall(13, 132, 1730, 586, (255, 0, 255))
+wall12 = Wall(150, 13, 1730, 705, (255, 0, 255))
+wall13 = Wall(13, 878, 2024, 1020, (255, 0, 255))
+wall14 = Wall(510, 173, 1524, 1147, (255, 0, 255))
+wall15 = Wall(220, 13, 2150, 1023, (255, 0, 255))
+wall16 = Wall(220, 13, 2488, 1023, (255, 0, 255))
+wall17 = Wall(13, 210, 2492, 1023, (255, 0, 255))
+wall18 = Wall(13, 350, 2353, 1023, (255, 0, 255))
+wall19 = Wall(200, 13, 2160, 1360, (255, 0, 255))
+wall20 = Wall(700, 13, 1330, 1988, (255, 0, 255))
+wall21 = Wall(13, 260, 1324, 1738, (255, 0, 255))
+wall22 = Wall(180, 13, 1154, 1738, (255, 0, 255))
+wall23 = Wall(300, 13, 1154, 1628, (255, 0, 255))
+wall24 = Wall(13, 160, 1154, 1738, (255, 0, 255))
+wall25 = Wall(320, 13, 838, 1883, (255, 0, 255))
+wall26 = Wall(13, 220, 1154, 1408, (255, 0, 255))
+wall27 = Wall(150, 305, 1034, 1168, (255, 0, 255))
+wall28 = Wall(150, 305, 764, 1168, (255, 0, 255))
+wall29 = Wall(150, 305, 1034, 768, (255, 0, 255))
+wall30 = Wall(150, 305, 764, 768, (255, 0, 255))
+wall31 = Wall(118, 13, 1463, 1880, (255, 0, 255))
+wall32 = Wall(337, 13, 1688, 1880, (255, 0, 255))
+wall33 = Wall(13, 268, 1463, 1630, (255, 0, 255))
+wall34 = Wall(13, 588, 1563, 1310, (255, 0, 255))
+wall35 = Wall(13, 145, 1692, 1740, (255, 0, 255))
+wall36 = Wall(145, 13, 1692, 1740, (255, 0, 255))
+wall37 = Wall(13, 185, 1910, 1475, (255, 0, 255))
+
+wall38 = Wall(50, 185, 2050, 1415, (255, 0, 255))
+wall39 = Wall(50, 120, 2100, 1385, (255, 0, 255))
+wall40 = Wall(70, 120, 2150, 1355, (255, 0, 255))
+wall41 = Wall(210, 70, 2150, 1355, (255, 0, 255))
+wall42 = Wall(250, 340, 2220, 1600, (255, 0, 255))
+wall43 = Wall(13, 320, 2575, 1365, (255, 0, 255))
+wall44 = Wall(80, 13, 2495, 1425, (255, 0, 255))
+wall45 = Wall(13, 120, 2495, 1315, (255, 0, 255))
+wall46 = Wall(170, 13, 2495, 1315, (255, 0, 255))
+wall47 = Wall(670, 13, 2495, 1215, (255, 0, 255))
+wall48 = Wall(400, 13, 2650, 1560, (255, 0, 255))
+wall49 = Wall(13, 260, 2660, 1320, (255, 0, 255))
+wall50 = Wall(13, 260, 3120, 1230, (255, 0, 255))
+wall51 = Wall(200, 100, 2800, 1350, (255, 0, 255))
+wall52 = Wall(640, 13, 2590, 1675, (255, 0, 255))
+wall53 = Wall(310, 150, 2580, 1785, (255, 0, 255))
+wall54 = Wall(13, 420, 2580, 1785, (255, 0, 255))
+wall55 = Wall(940, 13, 2180, 2195, (255, 0, 255))
+wall56 = Wall(13, 30, 2020, 1995, (255, 0, 255))
+
+wall57 = Wall(230, 180, 2300, 470, (255, 0, 255))  # Main Room
+wall58 = Wall(230, 180, 2075, 230, (255, 0, 255))
+wall59 = Wall(230, 180, 2515, 215, (255, 0, 255))
+wall60 = Wall(230, 150, 2515, 720, (255, 0, 255))
+wall61 = Wall(230, 150, 2055, 720, (255, 0, 255))
+
+GroupWall.add(wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9, wall10, wall11, wall12, wall13,
+              wall14, wall15, wall16, wall17, wall18, wall19, wall20, wall21, wall22, wall23, wall24, wall25,
+              wall26, wall27, wall28, wall29, wall30, wall31, wall32, wall33, wall34, wall35, wall36, wall37,
+              wall38, wall39, wall40, wall41, wall42, wall43, wall44, wall45, wall46, wall47, wall48, wall49,
+              wall50, wall51, wall52, wall53, wall54, wall55, wall56, wall57, wall58, wall59, wall60, wall61)
+
+GroupCrewmate = pygame.sprite.Group()
+
+crewmate1 = Crewmate("images/sprites/red_sprite.png", 1000, 100, width_sprite, height_sprite, 3)
+crewmate2 = Crewmate("images/sprites/red_sprite.png", 900, 100, width_sprite, height_sprite, 3)
+crewmate3 = Crewmate("images/sprites/red_sprite.png", 800, 100, width_sprite, height_sprite, 3)
+
+GroupCrewmate.add(crewmate1, crewmate2, crewmate3)
+
+impostor = Impostor("images/sprites/white.png", 0, 0, width_sprite, height_sprite, 4)
+
+player = Player('images/sprites/red_sprite.png', WIDTH // 2, HEIGHT // 2 - height_sprite,
+                width_sprite, height_sprite, 5)
+
+cam_x = -player.rect.x - WIDTH // 2 - 1050
+cam_y = -player.rect.y - HEIGHT // 2 + 350
+
+
+def game_run():
+    map_image = pygame.image.load("images/map/The_Skeld_map.webp")
+
+    scale_factor = 0.5
+    scaled_map = pygame.transform.scale(map_image,
+                                        (map_image.get_width() * scale_factor, map_image.get_height() * scale_factor))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.blit(scaled_map, (cam_x, cam_y))
+
+        player.reset()
+        player.update()
+
+        GroupWall.update()
+
+        crewmate1.reset()
+        crewmate1.update(player.rect)
+
+        impostor.reset()
+        impostor.update(crewmate1.rect, player.rect)
+
+        collide = pygame.sprite.spritecollide(impostor, GroupCrewmate, False, False)
+        for crewmate in collide:
+            if collide:
+                crewmate.speed = 0
+                crewmate.animation(kind='dead')
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+    pygame.quit()
+    sys.exit()
+
+
+def game_menu():
+    # pygame.mixer.init()
+    # pygame.mixer.music.load('music/among_us_impostor.mp3')
+    # pygame.mixer.music.play()
+
+    map_image = pygame.image.load("images/map/main_menu.jpg")
+    transform_map = pygame.transform.scale(map_image, (WIDTH, HEIGHT))
+
+    play_btn = Button(115, 50, (100, 0, 0), (150, 0, 0), screen)
+    settings_btn = Button(205, 50, (100, 0, 0), (150, 0, 0), screen)
+    quit_btn = Button(105, 50, (100, 0, 0), (150, 0, 0), screen)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.blit(transform_map, (0, 0))
+
+        play_btn.draw(screen, WIDTH // 2 - 57.5, HEIGHT - 535, "Play", game_run, 35)
+        settings_btn.draw(screen, WIDTH // 2 - 102.5, HEIGHT - 485, "Settings", settings, 35)
+        quit_btn.draw(screen, WIDTH // 2 - 52.5, HEIGHT - 435, "Quit", close, 35)
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+    pygame.quit()
+    sys.exit()
+
+
+def settings():
+    map_image = pygame.image.load("images/map/main_menu.jpg")
+    transform_map = pygame.transform.scale(map_image, (WIDTH, HEIGHT))
+
+    back_btn = Button(115, 45, (100, 0, 0), (150, 0, 0), screen)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.blit(transform_map, (0, 0))
+
+        back_btn.draw(screen, WIDTH // 2 - 370, HEIGHT - 570, "Back", game_menu, 30)
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+    pygame.quit()
+    sys.exit()
+
+
+def lobby():
+    map_image = pygame.image.load("images/map/lobby.png")
+    transform_map = pygame.transform.scale(map_image, (WIDTH, HEIGHT))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.blit(transform_map, (0, 0))
+
+        player.reset()
+        player.update()
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+    pygame.quit()
+    sys.exit()
+
+
+def close():
+    click = pygame.mouse.get_pressed()
+    if click[0] == 1:
+        sys.exit()
+
+
+game_menu()
